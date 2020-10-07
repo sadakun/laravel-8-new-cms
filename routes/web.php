82c1cc899a14c553f46a2d09917c96c0db205870
sadakun <sm123kuncoro@gmail.com>
2020-10-07 15:33:53 +0700

    install ui bootstrap, condition auth, sb admin for backend, create dummy with relationship user and post db using factory

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');
